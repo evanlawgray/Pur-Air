6368de02c431ed7086f414ad8b716e88f53dd46d
evanlawgray <evanlawgray@gmail.com>
2016-09-12 20:49:35 -0700

    Added Navbar to About Page
    Had to change class names of nav items to match those in the .index.
    Some cosmetic changes were made to the function as well.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,8 @@ $(function() {
 	var $navbarIsOpen = false;
 	var $navbarButtonIcon = $(".toggle-sidebar-button-icon");
 	var $icon = $(".fa");
+	// extra variables to try to fix nav on subpages
+	var $navbarItems = $(".header-nav").children; //NEW CODE
 	//hide the sidebar on initial page load
 	$navbar.css("height", "0px")
 	// create a function to be executed when the toggle button is clicked
@@ -13,15 +15,18 @@ $(function() {
 		var $navbarHeight;
 		var $navbarPadding;
 
+	$navbar.addClass(".hidden"); //NEW CODE
+
 		if ($navbarIsOpen) {
 			$navbarHeight = "0px";
 			$icon.removeClass("fa-times").addClass("fa-bars");
 			$navbarPadding = "0px";
 
 		} else {
+			$navbar.removeClass(".hidden");  //NEW CODE
 			$navbarHeight = "40px";
 			$icon.removeClass("fa-bars").addClass("fa-times");
-			$navbarPadding = "15px 50px"
+			$navbarPadding = "15px 50px";
 		}
 
 		$navbar.animate({
