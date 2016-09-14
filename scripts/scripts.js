ef72989e6da4512ccda19175fa19d70f3b7bf884
evanlawgray <evanlawgray@gmail.com>
2016-09-13 19:13:36 -0700

    Toggle Color of Navbar Button Icon
    Using a CSS transition to achieve animation effect.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,22 @@
 $(function() {
+
+	// MAKE NAVBAR STICK TO TOP OF PAGE WHEN SCROLLING
+
+/*	$(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      //console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 111) {
+      $('header').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 112) {
+      $('header').removeClass('navbar-fixed');
+    }
+  });   */
+
+	// MAKE NAVBAR DROP DOWN WHEN MENU ICON IS PRESSED
+
 	// get the toggle button as a variable
 	var $toggle = $(".toggle-sidebar-button");
 	var $navbar = $(".header-nav");
@@ -8,27 +26,27 @@ $(function() {
 
 
 	//hide the sidebar on initial page load
-	$navbar.css("height", "0px")
+	$navbar.css("height", "0px");
 	// create a function to be executed when the toggle button is clicked
 	$toggle.on("click", function() {
-		//Animate it open
+		//Animate the sidebar open
 		var $navbarHeight;
-		//var $navbarPadding;
 
 		if ($navbarIsOpen) {
 			$navbarHeight = "0px";
-			$icon.removeClass("fa-times").addClass("fa-bars");
-			//$navbarPadding = "0px";
+			$icon.removeClass("toggle-sidebar-button-active")
+			.removeClass("fa-times")
+			.addClass("fa-bars");
 
 		} else {
 			$navbarHeight = "40px";
-			$icon.removeClass("fa-bars").addClass("fa-times");
-			//$navbarPadding = "15px 50px";
+			$icon.addClass("toggle-sidebar-button-active")
+			.removeClass("fa-bars")
+			.addClass("fa-times");
 		}
 
 		$navbar.animate({
 			height: $navbarHeight,
-			//padding: $navbarPadding
 		}, 300);
 		$navbarIsOpen = !$navbarIsOpen;
 		});
